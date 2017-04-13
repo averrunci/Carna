a70162d8330402688010ccae87c1d9ded78df2d3
averrunci <averrunci.f@gmail.com>
2017-04-13 12:28:01 +0900

    Update version.

--- ReleaseNote.md
+++ ReleaseNote.md
@@ -1,5 +1,28 @@
 # Release note
 
+## v1.1.0
+
+### Add
+
+- Add Carna.UwpRunner.
+
+#### Carna
+
+- Add SampleAttribute.
+- Add ISampleDataSource.
+
+#### Carna.Runner
+
+- Add the function to create a fixture specified with the SampleAttribute.
+
+#### Carna.ConsoleRunner
+
+- Add ICarnaRunnerCommandLineParser.
+
+### Changes
+
+- Change EndTime of FixtureResults from DateTime.MaxValue to DateTime.MinValue when the number of FixtureResults is zero.
+
 ## v1.0.1
 
 ### Bug fix
--- Source/Carna.UwpRunner/Properties/AssemblyInfo.cs
+++ Source/Carna.UwpRunner/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Carna UWP Runner")]
-[assembly: AssemblyDescription("The runner of Carna testing framework for Universal Windows Platform application")]
+[assembly: AssemblyDescription("The runner of Carna testing and specification framework for Universal Windows Platform application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Carna UWP Runner")]
@@ -27,6 +27,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
 [assembly: ComVisible(false)]
