c0c0a362a7e4afc5138bbc1687beb0d2d1fdc89b
averrunci <averrunci@fievus.com>
2018-01-20 17:08:27 +0900

    Add UICultureAttribute.

---
+++ Source/Carna/UICultureAttribute.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (C) 2018 Fievus
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Carna
+{
+    /// <summary>
+    /// Specifies a UI culture during running a fixture.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class UICultureAttribute : AroundFixtureAttribure
+    {
+        /// <summary>
+        /// Gets a UI culture during running a fixture.
+        /// </summary>
+        public CultureInfo UICulture { get; }
+
+        private CultureInfo OriginalUICulture { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CultureAttribute"/> class
+        /// with the specified name of the UI culture.
+        /// </summary>
+        /// <param name="uiCultureName">The name of the UI culture.</param>
+        public UICultureAttribute(string uiCultureName)
+        {
+            UICulture = new CultureInfo(uiCultureName, false);
+        }
+
+        /// <summary>
+        /// Stores the current UI culture and replaces it with the UI culture specified in the constructor.
+        /// </summary>
+        /// <param name="context">The context of the fixture.</param>
+        public override void OnFixtureRunning(IFixtureContext context)
+        {
+            OriginalUICulture = Thread.CurrentThread.CurrentUICulture;
+            Thread.CurrentThread.CurrentUICulture = UICulture;
+            CultureInfo.CurrentUICulture.ClearCachedData();
+        }
+
+        /// <summary>
+        /// Restores the UI culture that is one before running the fixture.
+        /// </summary>
+        /// <param name="context">The context of the fixture.</param>
+        public override void OnFixtureRun(IFixtureContext context)
+        {
+            Thread.CurrentThread.CurrentUICulture = OriginalUICulture;
+            CultureInfo.CurrentUICulture.ClearCachedData();
+        }
+    }
+}
---
+++ Spec/Carna.Spec/UICultureAttributeSpec.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (C) 2018 Fievus
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+using System.Globalization;
+using System.Threading;
+
+using NSubstitute;
+
+namespace Carna
+{
+    [Specification("UICultureAttribute Spec")]
+    class UICultureAttributeSpec : FixtureSteppable
+    {
+        UICultureAttribute UICultureAttribute { get; } = new UICultureAttribute("fr-FR");
+
+        IFixtureContext FixtureContext = Substitute.For<IFixtureContext>();
+        CultureInfo CurrentUICultureInfo { get; } = Thread.CurrentThread.CurrentUICulture;
+
+        [Example("Sets the UI Culture on running a fixture")]
+        void Ex01()
+        {
+            When("to occur before running a fixture", () => UICultureAttribute.OnFixtureRunning(FixtureContext));
+            Then("the current UI culture should be changed to the specified culture", () => Thread.CurrentThread.CurrentUICulture == UICultureAttribute.UICulture);
+            When("to occur after running a fixture", () => UICultureAttribute.OnFixtureRun(FixtureContext));
+            Then("the current UI culture should be restored to the value before running a fixture", () => Thread.CurrentThread.CurrentUICulture == CurrentUICultureInfo);
+        }
+    }
+}
