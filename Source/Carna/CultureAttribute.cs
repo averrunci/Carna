2f2c1794d1009c0fb2763e483d074aaf0261c86f
averrunci <averrunci@fievus.com>
2018-01-20 17:06:45 +0900

    Add CultureAtrribute.

---
+++ Source/Carna/CultureAttribute.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (C) 2018 Fievus
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Carna
+{
+    /// <summary>
+    /// Specifies a culture during running a fixture.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class CultureAttribute : AroundFixtureAttribure
+    {
+        /// <summary>
+        /// Gets a culture during running a fixture.
+        /// </summary>
+        public CultureInfo Culture { get; }
+
+        private CultureInfo OriginalCulture { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CultureAttribute"/> class
+        /// with the specified name of the culture.
+        /// </summary>
+        /// <param name="cultureName">The name of the culture.</param>
+        public CultureAttribute(string cultureName)
+        {
+            Culture = new CultureInfo(cultureName, false);
+        }
+
+        /// <summary>
+        /// Stores the current culture and replaces it with the culture specified in the constructor.
+        /// </summary>
+        /// <param name="context">The context of the fixture.</param>
+        public override void OnFixtureRunning(IFixtureContext context)
+        {
+            OriginalCulture = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = Culture;
+            CultureInfo.CurrentCulture.ClearCachedData();
+        }
+
+        /// <summary>
+        /// Restores the culture that is one before running the fixture.
+        /// </summary>
+        /// <param name="context">The context of the fixture.</param>
+        public override void OnFixtureRun(IFixtureContext context)
+        {
+            Thread.CurrentThread.CurrentCulture = OriginalCulture;
+            CultureInfo.CurrentCulture.ClearCachedData();
+        }
+    }
+}
---
+++ Spec/Carna.Spec/CultureAttributeSpec.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (C) 2018 Fievus
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+using System.Globalization;
+using System.Threading;
+
+using NSubstitute;
+
+namespace Carna
+{
+    [Specification("CultureAttribute Spec")]
+    class CultureAttributeSpec : FixtureSteppable
+    {
+        CultureAttribute CultureAttribute { get; } = new CultureAttribute("fr-FR");
+
+        IFixtureContext FixtureContext = Substitute.For<IFixtureContext>();
+        CultureInfo CurrentCultureInfo { get; } = Thread.CurrentThread.CurrentCulture;
+
+        [Example("Sets the Culture on running a fixture")]
+        void Ex01()
+        {
+            When("to occur before running a fixture", () => CultureAttribute.OnFixtureRunning(FixtureContext));
+            Then("the current culture should be changed to the specified culture", () => Thread.CurrentThread.CurrentCulture == CultureAttribute.Culture);
+            When("to occur after running a fixture", () => CultureAttribute.OnFixtureRun(FixtureContext));
+            Then("the current culture should be restored to the value before running a fixture", () => Thread.CurrentThread.CurrentCulture == CurrentCultureInfo);
+        }
+    }
+}
