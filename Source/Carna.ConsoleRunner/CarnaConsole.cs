012792f15f2e42c704a5bbc9c5ac6cf103c14b98
averrunci <averrunci@fievus.com>
2021-03-29 13:31:12 +0900

    Change the foreground color of the Ready state to the DarkGray.

--- Source/Carna.ConsoleRunner/CarnaConsole.cs
+++ Source/Carna.ConsoleRunner/CarnaConsole.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2017-2018 Fievus
+﻿// Copyright (C) 2017-2021 Fievus
 //
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
@@ -48,11 +48,11 @@ namespace Carna.ConsoleRunner
         public static void WriteSuccess(object value) => Write(ConsoleColor.Green, value);
         public static void WriteSuccess(string format, params object[] args) => Write(ConsoleColor.Green, format, args);
 
-        public static void WriteLineReady(object value) => WriteLine(ConsoleColor.Gray, value);
-        public static void WriteLineReady(string format, params object[] args) => WriteLine(ConsoleColor.Gray, format, args);
+        public static void WriteLineReady(object value) => WriteLine(ConsoleColor.DarkGray, value);
+        public static void WriteLineReady(string format, params object[] args) => WriteLine(ConsoleColor.DarkGray, format, args);
 
-        public static void WriteReady(object value) => Write(ConsoleColor.Gray, value);
-        public static void WriteReady(string format, params object[] args) => Write(ConsoleColor.Gray, format, args);
+        public static void WriteReady(object value) => Write(ConsoleColor.DarkGray, value);
+        public static void WriteReady(string format, params object[] args) => Write(ConsoleColor.DarkGray, format, args);
 
         public static void WriteLinePending(object value) => WriteLine(ConsoleColor.Yellow, value);
         public static void WriteLinePending(string format, params object[] args) => WriteLine(ConsoleColor.Yellow, format, args);
