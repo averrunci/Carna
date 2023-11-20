6c2399bdc8bb8c92fe4b27abbe0ce22ee542d0bd
averrunci <averrunci@fievus.com>
2023-11-20 15:16:08 +0900

    Obsolete the Window property of the CarnaWinUIRunner class and add the InvokeAsync method to the CarnaWinUIRunner class.

--- Source/Carna.WinUIRunner/CarnaWinUIRunner.cs
+++ Source/Carna.WinUIRunner/CarnaWinUIRunner.cs
@@ -1,10 +1,11 @@
-﻿// Copyright (C) 2022 Fievus
+﻿// Copyright (C) 2022-2023 Fievus
 //
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
 using Carna.Runner;
 using Carna.Runner.Formatters;
 using Carna.WinUIRunner.Converters;
+using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 
 namespace Carna.WinUIRunner;
@@ -17,8 +18,43 @@ public static class CarnaWinUIRunner
     /// <summary>
     /// Gets the currently activated window for the CarnaWinUIRunner.
     /// </summary>
+    [Obsolete("This method is obsolete. Use the InvokeAsync method instead if you want to run a task on the thread associated with the DispatcherQueue asynchronously.")]
     public static Window Window => window ?? throw new InvalidOperationException("The window instance is not instantiated.");
     private static Window? window;
+
+    /// <summary>
+    /// Invokes the specified task on the thread associated with the <see cref="DispatcherQueue"/> asynchronously.
+    /// </summary>
+    /// <param name="action">The delegate to the task to invoke.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public static Task InvokeAsync(DispatcherQueueHandler action) => Window.DispatcherQueue.RunAsync(action);
+
+    /// <summary>
+    /// Invokes the specified task on the thread associated with the <see cref="DispatcherQueue"/>
+    /// with the specified priority asynchronously.
+    /// </summary>
+    /// <param name="priority">The priority of the task (such as Low, Normal, or High).</param>
+    /// <param name="action">The delegate to the task to invoke.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public static Task InvokeAsync(DispatcherQueuePriority priority, DispatcherQueueHandler action) => Window.DispatcherQueue.RunAsync(priority, action);
+
+    /// <summary>
+    /// Invokes the specified task on the thread associated with the <see cref="DispatcherQueue"/> asynchronously.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result returned by the task.</typeparam>
+    /// <param name="action">The delegate to the task to invoke.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public static Task<TResult> InvokeAsync<TResult>(Func<TResult> action) => Window.DispatcherQueue.RunAsync(action);
+
+    /// <summary>
+    /// Invokes the specified task on the thread associated with the <see cref="DispatcherQueue"/>
+    /// with the specified priority asynchronously.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result returned by the task.</typeparam>
+    /// <param name="priority">The priority of the task (such as Low, Normal, or High).</param>
+    /// <param name="action">The delegate to the task to invoke.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public static Task<TResult> InvokeAsync<TResult>(DispatcherQueuePriority priority, Func<TResult> action) => Window.DispatcherQueue.RunAsync(priority, action);
 
     /// <summary>
     /// Runs fixtures.
--- Spec/Carna.WinUIRunner.Spec/Converters/FixtureStatusToBrushConverterSpec.cs
+++ Spec/Carna.WinUIRunner.Spec/Converters/FixtureStatusToBrushConverterSpec.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2022 Fievus
+﻿// Copyright (C) 2022-2023 Fievus
 //
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
@@ -20,7 +20,7 @@ class FixtureStatusToBrushConverterSpec : FixtureSteppable
     [Sample(Source = typeof(FixtureStatusBrushSampleDataSource))]
     async Task Ex01(object value, object expected)
     {
-        await CarnaWinUIRunner.Window.DispatcherQueue.RunAsync(() =>
+        await CarnaWinUIRunner.InvokeAsync(() =>
         {
             if (expected.Equals(Colors.Transparent))
             {
